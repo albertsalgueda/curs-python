0b5547b2054ea877d621dff47a72fd4b3c6fa1fe
Oscar Manzaneda Torrente <oscar.manzaneda@gmail.com>
2023-04-19 05:55:46 +0200

    Correcció if

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import sim_humitat
 import activ_riego
 
 pluja=temps_status.pluja
+print(pluja)
 
-if pluja > 0 or sim_humitat > 95:
+if pluja > 0 or sim_humitat.sim_humitat > 95:
     activ_riego.riego = False
 
 else:
